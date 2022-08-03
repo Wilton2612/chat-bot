82234ad890bc4bf7a1d944bba0cfc249e5376d6e
Wilton2612 <wiltonestebanm2001@gmail.com>
2022-08-03 18:34:28 -0500

    avance

--- bot2.py
+++ bot2.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -14,19 +15,36 @@ def validar_qr():
         return False
     return True
 
-def buscar_chat(nombre : str):
-    buscando = True
 
-    while buscando:
-        print("BUSCANDO CHAT")
-        elements = browser.find_elements(By.TAG_NAME, 'span')
-        for element in elements:
-            if element.text == nombre:
-                print("Chat encontrado")
-                element.click()
-                buscando = False
-                break
 
+def validar_mensaje_leido(elem):
+    try:
+        elem.find_element(By.CLASS_NAME, '_1pJ9J')
+    except Exception as e:
+        return False
+    return True
+
+
+
+
+def buscar_chat():
+   
+    if len(browser.find_elements(By.CLASS_NAME, 'zaKsw'))==0:
+        print("chat abierto")
+
+    print("BUSCANDO MENSAJES RECIBIDOS")
+    elements_general = browser.find_elements(By.XPATH, '//*[@id="pane-side"]/div[2]/div/div/div[12]')
+
+    for element in elements_general:
+        print("buscando mensajes sin leer")
+
+        chat_abierto = element.find_elements(By.CLASS_NAME, '_1pJ9J')
+
+        if len(chat_abierto) == 0:
+            print("Ya se leyo el chat")
+
+        element.click()
+        print("chat abierto")
 
 
 
@@ -37,12 +55,9 @@ def enviar_mensaje(mensaje:str):
     chat.send_keys(mensaje+ Keys.ENTER)
     print("SE ESCRIBIÓ EL MENSAJE")
     sleep(2)
-
-    """
     enviar = browser.find_element(By.TAG_NAME, 'button')
     enviar.click()
     print("SE ENVIO EL MENSAJE")
-    """
 
 def bot_whatsapp():
 
@@ -59,10 +74,9 @@ def bot_whatsapp():
             print("iniciamos sesión")
             break
     
-    sleep(20)
-    buscar_chat("Hermanita Heidy")
-    sleep(5)
-    enviar_mensaje("Hola")
+    sleep(10)
+    buscar_chat()
+
         
 
 bot_whatsapp()
